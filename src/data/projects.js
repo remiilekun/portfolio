8af39a5aa3423e5ae0cf8323730ee1e77d5cde10
Remilekun Salami <remilekun.salami@gmail.com>
2021-05-14 00:52:36 +0100

    chore: project descriptions

--- src/data/projects.js
+++ src/data/projects.js
@@ -36,8 +36,7 @@ export const projects = [
   },
   {
     coverImage: '/assets/img/projects/udux.jpg',
-    description:
-      "uduX is a music streaming platform. I worked with a team of software engineers on uduX, by translating designs and consuming apis, my role was to build applications that enables ease of use of streaming and accessibility of user's uduX data for both platforms, web and mobile( iOS ).",
+    description: "uduX is Nigeria's first indigenous subscription-based music store and streaming service platform.",
     logo: UduXIcon,
     name: 'uduX',
     technologies: ['html', 'react', 'reactNative', 'swift'],
@@ -50,7 +49,7 @@ export const projects = [
   {
     coverImage: '/assets/img/projects/find-maids.jpg',
     description:
-      'A web app used to book cleaning appointments by customers while the admin are able to manage their staffs. I built the UI components using the designs handed over to me and also handled the UI logic functionality of the app.',
+      'A web app used to book cleaning appointments by customers while the admin are able to manage their staffs.',
     logo: FindmaidsIcon,
     name: 'Find Maids',
     technologies: ['html', 'react'],
@@ -61,8 +60,7 @@ export const projects = [
   },
   {
     coverImage: '/assets/img/projects/git-explorer.jpg',
-    description:
-      'Gitexplorer is an open source project created by Summitech to help people with git commands. While working with a team of a designer and a backend engineer, I was tasked with creating the UI component and also deliberated with the backend engineer on a performant logic of data management.',
+    description: 'Gitexplorer is an open source project created by Summitech to help people with git commands.',
     logo: GitExplorerIcon,
     name: 'Git Explorer',
     technologies: ['html', 'react'],
@@ -73,8 +71,7 @@ export const projects = [
   },
   {
     coverImage: '/assets/img/projects/chiji.png',
-    description:
-      'A bitcoin and gift card trading application. I translated the designs assigned to me to pixel perfect mobile screens, frontend logic, consumed apis, and I also worked on a chat system using socket.io to enable instant messaging between and user and an agent to allow easy and instant trades.',
+    description: 'A bitcoin and gift card trading application.',
     logo: Chiji14XchangeIcon,
     name: 'Chiji14xchange',
     technologies: ['react', 'reactNative'],
