3cbae6d3f1015c644bcb8d57c243b8ed2b2d0199
Remilekun Salami <remilekun.salami@gmail.com>
2022-12-16 21:24:46 +0100

    chore(perf): remove date-fns

--- src/lib/date.js
+++ src/lib/date.js
@@ -1,11 +1,21 @@
-import { parse, format } from 'date-fns';
+const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-export const parseCompanyDate = date => {
-  return parse(date, 'yyyy-MM-dd', new Date());
+export const parseCompanyDate = dateString => {
+  const dateRegex = /(\d{4})-(\d{2})-(\d{2})/;
+  const match = dateRegex.exec(dateString);
+  if (match) {
+    const year = match[1];
+    const month = match[2] - 1;
+    const day = match[3];
+    return new Date(year, month, day);
+  }
+  return null;
 };
 
 export const formatCompanyDate = date => {
-  return format(date, 'MMM yyyy');
+  const month = monthNames[date.getMonth()];
+  const year = date.getFullYear();
+  return `${month} ${year}`;
 };
 
 export const parseAndFormatCompanyDate = date => {
