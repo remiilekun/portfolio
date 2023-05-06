6957546077a4e54cad857101e045c5a18e8d02a4
Remilekun Salami <remilekun.salami@gmail.com>
2023-05-06 20:46:19 +0100

    fix: media query types

--- src/theme/media-query.tsx
+++ src/theme/media-query.tsx
@@ -1,11 +1,18 @@
 import { css } from '@emotion/react';
 import { breakpoints } from './breakpoints';
 
-export const mq = Object.keys(breakpoints).reduce((acc, label) => {
-  acc[label] = (...args) => css`
-    @media (min-width: ${breakpoints[label]}) {
+type BreakpointsType = typeof breakpoints;
+type BreakpointsKey = keyof BreakpointsType;
+
+export type MQ = {
+  [key in keyof BreakpointsType]: (strings: TemplateStringsArray, ...args: any[]) => string;
+};
+
+export const mq = (Object.keys(breakpoints).reduce((acc: any, label: string) => {
+  acc[label] = (...args: any[]) => css`
+    @media screen and (min-width: ${breakpoints[label as BreakpointsKey]}) {
       ${css(...args)}
     }
   `;
   return acc;
-}, {});
+}, {}) as unknown) as MQ;
